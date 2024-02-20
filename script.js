ca3bd6513baceaa55c0173947e837b2583fe99aa
Sh Raj <shaswatraj3@gmail.com>
2024-02-20 21:59:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const fileInput = document.getElementById('fileInput');
+    const uploadButton = document.getElementById('uploadButton');
+    const responseDiv = document.getElementById('response');
+    const uploadedImage = document.getElementById('uploadedImage');
+
+    uploadButton.addEventListener('click', async function () {
+        const file = fileInput.files[0];
+        if (!file) {
+            responseDiv.textContent = 'Please select a file.';
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try {
+            const response = await fetch('https://discord.com/api/webhooks/1209461004518432778/zMKaNOteB0b0M3Aq7WykfAk2KkODHFISU9Uh6GG7pvBJF1PGsYfSfpuB4TJgegtlnmnt', {
+                method: 'POST',
+                body: formData
+            });
+            const responseData = await response.json();
+          console.log(responseData)
+            responseDiv.textContent = `Image uploaded successfully. URL: ${responseData.attachments[0].url}`;
+            uploadedImage.src = responseData.attachments[0].url;
+        } catch (error) {
+            console.error('Error uploading image:', error);
+            responseDiv.textContent = 'Error uploading image.';
+        }
+    });
+});
